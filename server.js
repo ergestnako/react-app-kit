93e44b5aaee0c8d0160434d8f2d675c18c556b72
Bucky Maler <maler.bucky@gmail.com>
2017-03-27 00:45:02 -0500

    Modified static assets path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('views', '.')
 app.engine('html', renderFile)
 app.set('view engine', 'html')
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
