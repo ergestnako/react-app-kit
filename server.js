969ce240c2bd726a2002c37ef106a6cbee9e329f
Bucky Maler <maler.bucky@gmail.com>
2017-03-27 01:32:09 -0500

    Remove unnecessary OR operator

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const renderFile = require('ejs').renderFile
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.set('port', PORT)
 app.set('views', '.')
@@ -19,6 +19,6 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.listen(PORT,function() {
+app.listen(PORT, function() {
   console.log('\n=== Listening on port ' + PORT + ' ===\n')
 })
