7098400147b841696d2b9400b75c478477e752f2
Bucky Maler <maler.bucky@gmail.com>
2017-03-28 01:33:27 -0500

    Remove unused code

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.set('port', PORT)
-// app.set('views', '.')
 app.engine('html', renderFile)
 app.set('view engine', 'html')
 
@@ -14,10 +13,6 @@ app.use(express.static(__dirname + '/dist'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// app.get('/dist', function (req, res) {
-//   res.render('index')
-// })
 
 app.listen(PORT, function() {
   console.log('\n=== Listening on port ' + PORT + ' ===\n')
